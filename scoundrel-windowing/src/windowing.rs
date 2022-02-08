b0a6b045b10f93c5b2422773325faeb8697a22f0
Miroslav <gavrilov.miroslav@gmail.com>
2022-02-08 17:34:40 +0100

    Added frame skipping and random color changes to test out speed bumps.

--- scoundrel-windowing/src/windowing.rs
+++ scoundrel-windowing/src/windowing.rs
@@ -6,8 +6,11 @@ use crow::glutin::event::{ElementState, Event, MouseButton, VirtualKeyCode, Wind
 use crow::glutin::event_loop::{ControlFlow, EventLoop};
 use crow::glutin::platform::windows::EventLoopExtWindows;
 use crow::glutin::window::WindowBuilder;
+use rand::Rng;
 use scoundrel_common::keycodes::{KeyState, MouseState};
 use scoundrel_common::engine_context::EngineContext;
+
+const SKIP_FRAME: u64 = 10;
 
 fn transmute_keycode(vk: VirtualKeyCode) -> KeyState {
     unsafe { std::mem::transmute(vk) }
@@ -19,8 +22,10 @@ fn transmute_mouse(mb: MouseButton, st: ElementState) -> MouseState {
 
 pub fn window_event_loop(shared_data: EngineContext) {
     let mut event_loop = EventLoop::new_any_thread();
-
+    let mut frame = 0u64;
     let mut context = Context::new(WindowBuilder::new(), &event_loop).unwrap();
+
+    let mut rng = rand::thread_rng();
 
     let texture = Texture::load(&mut context, "./textures/8x8glyphs.png").unwrap();
     let textures_by_char = {
@@ -35,13 +40,8 @@ pub fn window_event_loop(shared_data: EngineContext) {
         map_index
     };
 
-    let mut frame = 0;
-
-
     event_loop.run(move |event: Event<()>, _window_target: _, control_flow: &mut ControlFlow| match event {
-        Event::WindowEvent {
-            event: WindowEvent::CloseRequested, ..
-        } => {
+        Event::WindowEvent { event: WindowEvent::CloseRequested, .. } => {
             *control_flow = ControlFlow::Exit;
             shared_data.should_quit.store(true, Ordering::SeqCst);
 
@@ -49,23 +49,31 @@ pub fn window_event_loop(shared_data: EngineContext) {
         },
 
         Event::MainEventsCleared => {
-            frame += 1;
-            context.window().request_redraw();
-            shared_data.frame_counter.fetch_add(1, Ordering::Relaxed);
-        },
+            if frame % SKIP_FRAME == 0 {
+                let mut surface = context.surface();
+                context.clear_color(&mut surface, (0.0, 0.0, 0.0, 1.0));
 
-        Event::RedrawRequested(_) => {
-            let mut surface = context.surface();
-            context.clear_color(&mut surface, (0.0, 0.0, 0.0, 1.0));
+                let mut config = DrawConfig::default();
+                config.scale = (2, 2);
+                for i in (0..1024).step_by(16) {
+                    for j in (0..768).step_by(16) {
 
-            let mut config = DrawConfig::default();
-            config.scale = (2, 2);
-            for i in (0..1024).step_by(16) {
-                for j in (0..768).step_by(16) {
-                    context.draw(&mut surface, &textures_by_char[&('.' as u16)], (i, j), &config);
+                        config.color_modulation = [
+                            [rng.gen::<f32>(), 0.0, 0.0, 0.0],
+                            [0.0, rng.gen::<f32>(), 0.0, 0.0],
+                            [0.0, 0.0, rng.gen::<f32>(), 0.0],
+                            [0.0, 0.0, 0.0, 1.0],
+                        ];
+
+                        context.draw(&mut surface, &textures_by_char[&(rng.gen_range(1u16..128u16))], (i, j), &config);
+                    }
                 }
+                context.present(surface).unwrap();
+
             }
-            context.present(surface).unwrap();
+
+            frame += 1;
+            shared_data.frame_counter.fetch_add(1, Ordering::SeqCst);
         },
 
         Event::WindowEvent { event: WindowEvent::KeyboardInput {input, .. }, .. } => {
